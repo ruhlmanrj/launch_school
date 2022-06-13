077be9b781eb460fc3d03bae7731ae88b1c506e1
Robert Ruhlman <RuhlmanRJ@gmail.com>
2022-06-12 20:58:39 -0400

    Updated directory structure

--- exercise_sets/js_fundamentals/functions/var_declarations.js
+++
@@ -1,6 +0,0 @@
-// What will the following code log to the console and why? 
-
-console.log(a); // => `undefined` since `var` declarations reference `undefined`
-                // until explicit assignment occurs
-
-var a = 1;      // Hoisted and scoped globally
