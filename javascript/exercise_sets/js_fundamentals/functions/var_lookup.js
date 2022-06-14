92a48801d2b021afa4f0b1d5121e78ebdd5457a5
Robert Ruhlman <RuhlmanRJ@gmail.com>
2022-06-14 17:20:42 -0400

    Restructuring repository layout

--- javascript/exercise_sets/js_fundamentals/functions/var_lookup.js
+++
@@ -1,9 +0,0 @@
-// What will the following code log to the console and why?
-
-var myVar = 'This is global'; // Has global scope
-
-function someFunction() {     // Closes over `myVar`
-  console.log(myVar);         // `myVar` refers to `'This is global'`
-}
-
-someFunction();               // => 'This is global'
--- javascript/exercise_sets/js_fundamentals/functions/var_scope.js
+++
@@ -1,8 +0,0 @@
-// What will the following code log to the console and why?
-
-function someFunction() {
-  myVar = 'This is global';   // `myVar` will be initialized with global scope.
-}
-
-someFunction();               // `myVar` is initialized
-console.log(myVar);           // Logs 'This is global'
