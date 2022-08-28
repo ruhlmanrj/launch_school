4f0af9728346e9210b767987eafc16bc446952a4
Robert Ruhlman <RuhlmanRJ@gmail.com>
2022-08-28 02:18:46 -0400

    JS230 Lesson 2 Problem

---
+++ frontend/courses/js230/lesson2_exercises/inputBox.js
@@ -0,0 +1,60 @@
+// Program that creates a custome input element
+
+document.addEventListener('DOMContentLoaded', inputElementProgram);
+
+function inputElementProgram() {
+  let cursorTogglerId;
+
+  const textField = document.querySelector('.text-field');
+  const textFieldContent = document.querySelector('.content');
+
+  const addFocusedClass = () => {
+    textField.classList.add('focused');
+  };
+
+  const removeFocusedClass = () => {
+    textField.classList.remove('focused');
+  };
+
+  const displayCursor = () => {
+    const toggleCursor = () => textField.classList.toggle('cursor');
+    cursorTogglerId ||= setInterval(toggleCursor, 500);
+  };
+
+  const removeCursor = () => {
+    clearInterval(cursorTogglerId);
+    cursorTogglerId = null;
+    textField.classList.remove('cursor');
+  };
+
+  const displayKeystroke = ({ key }) => {
+    const isNotFocused = !textField.classList.contains('focused');
+    if (isNotFocused) {
+      return;
+    }
+
+    if (key === 'Backspace') {
+      const content = textFieldContent.textContent;
+      const lastIndex = content.length - 1;
+      textFieldContent.textContent = content.slice(0, lastIndex);
+    }
+
+    if (key.length === 1) {
+      textFieldContent.textContent += key;
+    }
+  }
+
+  const textFieldClickCallbacks = (event) => {
+    event.stopPropagation();
+    [addFocusedClass, displayCursor].forEach(callback => callback(event));
+  };
+
+  const documentClickCallbacks = () => {
+    [removeFocusedClass, removeCursor].forEach(callback => callback());
+  };
+
+  textField.addEventListener('click', textFieldClickCallbacks);
+  document.addEventListener('click', documentClickCallbacks);
+  document.addEventListener('keydown', displayKeystroke);
+}
+
