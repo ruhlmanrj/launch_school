78ee5249563b192c0c2568a264782a9a0f767649
Robert Ruhlman <RuhlmanRJ@gmail.com>
2022-10-13 02:29:25 -0400

    Forms exercises

--- frontend/courses/js230/lesson2_exercises/guessingGame.js
+++ frontend/courses/js230/lesson2_exercises/guessingGame.js
@@ -1,88 +1,64 @@
 // A random number between 1 and 100 is generated, then guesses are accepted
 // from the client until the correct number is guessed.
 
-// Generate a random number between 1 and 100, inclusive.
-function randomNumber() {
-  const MINIMUM = 1;
-  const MAXIUMUM = 1;
-  
-  const difference = MAXIUMUM - MINIMUM;
-  return Math.floor((Math.random() * (difference + 1))) + MINIMUM;
+class GuessingGame {
+  constructor() {
+    this.cacheNodes();
+    this.resetGame();
+  }
+
+  cacheNodes() {
+    this.guessForm = document.querySelector('form');
+    this.guessMessage = document.querySelector('p');
+    this.guess = document.querySelector('#guess');
+    this.newGame = document.querySelector('a');
+  }
+
+  resetGame() {
+    this.gameOver = false;
+    this.guessForm.reset();
+    this.displayMessage('Choose a number:');
+    this.secretNumber = this.getSecretNumber();
+    this.addedListeners = [];
+    this.bind();
+  }
+
+  getSecretNumber() {
+    return ~~(Math.random() * 100) + 1;
+  }
+
+  bind() {
+    const handleGuess = e => {
+      e.preventDefault();
+      this.handleGuess();
+    }
+
+    this.guessForm.addEventListener('submit', handleGuess);
+    this.newGame.addEventListener('click', this.handleNewGame.bind(this));
+  }
+
+  handleGuess() {
+    if (this.gameOver) { return; }
+
+    const guess = Number(this.guess.value);
+    if (guess > this.secretNumber) {
+      this.displayMessage('Your guess is too high');
+    } else if (guess < this.secretNumber) {
+      this.displayMessage('Your guess is too low');
+    } else {
+      this.displayMessage('You guessed the number!');
+      this.gameOver = true;
+    }
+  }
+
+  handleNewGame(e) {
+    e.preventDefault();
+    this.resetGame();
+  }
+
+  displayMessage(message) {
+    this.guessMessage.textContent = message;
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const START_MESSAGE = 'Guess a number from 1 to 100';
-  const DISABLED_BUTTON_BACKGROUND = 'rgba(255, 136, 136, 0.8)';
-  const DISABLED_BUTTON_SHADOW = '1px 1px 5px inset red';
-
-  const input = document.getElementById('guess');
-  const form = document.querySelector('form');
-  const paragraph = document.getElementById('message');
-  const button = document.querySelector('[type="submit"]');
-  const link = document.getElementById('new-game');
-
-  let answer = randomNumber();
-  let guessCount = 0;
-  let guess;
-  let message;
-
-  function disableButton() {
-    button.setAttribute('disabled', true);
-    button.style.background = DISABLED_BUTTON_BACKGROUND;
-    button.style.boxShadow = DISABLED_BUTTON_SHADOW;
-  }
-
-  function enableButton() {
-    button.removeAttribute('disabled');
-    button.removeAttribute('style');
-  }
-
-  const setGuess = () => {
-    const preliminary = input.value;
-    guess = isNaN(preliminary) ?
-                   preliminary : Number(preliminary);
-  };
-
-  const setMessage = () => {
-    if (answer > guess) {
-      message = `My number is greater than ${String(guess)}`;
-    } else if (answer < guess) {
-      message = `My number is less than ${String(guess)}`;
-    } else if (answer === guess) {
-      const noun = (guessCount === 1 ? 'guess' : 'guesses');
-      message = `That's my number! It took you ${String(guessCount)} ${noun}.`;
-    } else {
-      message = `'${guess}' is not a number. Try again.`;
-    }
-  };
-
-  const displayMessage = () => {
-    paragraph.textContent = message;
-  };
-
-  const playTurn = (event) => {
-    event.preventDefault();
-
-    guessCount += 1;
-    [setGuess, setMessage, displayMessage].forEach(callback => {
-      callback();
-    });
-
-    if (guess === answer) {
-      disableButton();
-    }
-  }
-
-  const resetGame = () => {
-    guessCount = 0;
-    answer = randomNumber();
-    input.value = '';
-    enableButton();
-
-    message = START_MESSAGE;
-    displayMessage();
-  };
-
-  form.addEventListener('submit', playTurn);
-  link.addEventListener('click', resetGame);
-});
+new GuessingGame();
---
+++ frontend/exercise_sets/front_end_javascript/forms/signup/signup.js
@@ -0,0 +1,133 @@
+// sign_up.js
+
+const signup = {
+  init() {
+    this.form = document.querySelector('form');
+    this.cards = document.querySelector('#cards');
+    this.templates = {};
+    this.disallowedChars = {
+      'first-name': /[^a-zA-z'\s]/,
+      'last-name': /[^a-zA-z'\s]/,
+      'phone': /[^\d]/,
+      'card': /[^\d]/,
+    };
+    this.cacheTemplates();
+    this.bind();
+  },
+
+  cacheTemplates() {
+    const templates = document.querySelectorAll('[type="text/x-handlebars-template"]');
+    for (const template of templates) {
+      this.templates[template.dataset.id] = Handlebars.compile(template.text);
+    }
+  },
+
+  bind() {
+    this.form.addEventListener('focusin', this.handleFocusin);
+    this.form.addEventListener('focusout', this.handleFocusout.bind(this));
+    this.form.addEventListener('submit', this.handleSubmit.bind(this));
+    this.form.addEventListener('keydown', this.handlekeydown.bind(this));
+    this.cards.addEventListener('keyup', this.handleKeyupForCards);
+  },
+
+  handleKeyupForCards(e) {
+    const nextSib = e.target.nextElementSibling;
+    const keyIsNotDigit = !e.key.match(/\d/);
+    const isNotTabForwardable = e.target.tagName !== 'INPUT' || keyIsNotDigit ||
+                                !nextSib;
+
+    if (isNotTabForwardable) { return; }
+
+    const reachedMax = e.target.value.length >= e.target.maxLength;
+    if (reachedMax) {
+      nextSib.focus();
+    }
+  },
+
+  handleFocusin(e) {
+    if (e.target.tagName !== 'INPUT') { return; }
+
+    const nextSib = e.target.nextElementSibling;
+
+    if (nextSib && nextSib.dataset.type === 'error-message') {
+      nextSib.remove()
+      e.target.classList.remove('error');
+    }
+  },
+
+  handleFocusout(e) {
+    if (e.target.tagName !== 'INPUT') { return; }
+
+    if (this.form.checkValidity() && this.isMainErrorMessage()) {
+      this.form.children[0].remove();
+      return;
+    }
+
+    this.displayControlError(e.target);
+  },
+
+  handleSubmit(e) {
+    if (this.form.checkValidity()) { return; }
+    e.preventDefault();
+
+    this.displayControlErrors();
+
+    if (!this.isMainErrorMessage()) {
+      const message = 'Form cannot be submitted until errors are corrected.'
+      this.form.insertAdjacentHTML('afterbegin', this.templates.error(message));
+    }
+  },
+
+  handlekeydown(e) {
+    if (this.isInvalidControlKey(e.target, e.key)) {
+      e.preventDefault();
+    }
+  },
+
+  getValidityMessage(control) {
+    const validity = control.validity;
+    const purpose = control.dataset.for;
+    let message;
+
+    if (validity.valueMissing) {
+      message = `${purpose} is a required field.`;
+    } else if (validity.patternMismatch) {
+      message = `Please enter a valid ${purpose}.`;
+    } else if (validity.tooShort) {
+      const minLength = control.minLength;
+      message = `${purpose} must be at least ${minLength} characters long.`;
+    }
+
+    return message;
+  },
+
+  displayControlError(control) {
+    const message = this.getValidityMessage(control);
+
+    if (message) {
+      control.classList.add('error');
+      control.insertAdjacentHTML('afterend', this.templates.error(message));
+    }
+  },
+
+  displayControlErrors() {
+    for (const control of this.form.elements) {
+      const noErrorMessage = !control.nextElementSibling;
+
+      if (noErrorMessage) {
+        this.displayControlError(control);
+      }
+    }
+  },
+
+  isMainErrorMessage() {
+    return this.form.children[0].dataset.type === 'error-message';
+  },
+
+  isInvalidControlKey(control, key) {
+    const disallowedChars = this.disallowedChars[control.name];
+    return key.length === 1 && disallowedChars && key.match(disallowedChars);
+  },
+};
+
+signup.init();
